a631ef246c873e3733940637995af69e5b62485e
Timo Schneider <extern.schneider_timo@allianz.de>
2023-04-13 10:05:14 +0200

    Aufgabe9/Aufgabe1 Angepasst

--- src/main/java/aufgaben/aufgabe1/Two_Sum.java
+++ src/main/java/aufgaben/aufgabe1/Two_Sum.java
@@ -1,7 +1,26 @@
 package aufgaben.aufgabe1;
 
-public class Two_Sum {
-    public static void main(String[] args) {
+import java.util.HashMap;
 
+class Two_Sum {
+    public static void main(String[] args) {
+        System.out.println(twoSum(new int[]{2,3,5,7}, 8));
+    }
+    public static int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            int addition = target - nums[i];
+            if (map.containsKey(addition)) {
+                result[0] = i;
+                result[1] = map.get(addition);
+                return result;
+            }
+            map.put(nums[i], i);
+        }
+        return result;
     }
 }
---
+++ src/main/java/aufgaben/aufgabe9/Palidrom_Number.java
@@ -0,0 +1,22 @@
+package aufgaben.aufgabe9;
+
+class Palidrom_Number {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(212));
+    }
+
+    public static boolean isPalindrome(int x) {
+        int org = x;
+        int reversed = 0;
+
+        if(x < 0) return false;
+
+        while (x != 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+        return reversed == org;
+
+    }
+}
